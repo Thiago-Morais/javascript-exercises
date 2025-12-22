2ef66901c17d375d1c03e807fe6051240aa5e807
Cyber Falcon <thiolimor@gmail.com>
2025-12-22 18:08:00 -0300

    feat(09): Pass ninth exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  const min = a < b ? a : b;
+  const max = a < b ? b : a;
+  const diff = max - min;
+  let total = min;
+
+  for (let i = 0; i < diff; i++) {
+    total += min + i + 1;
+  }
+
+  return total;
 };
 
+const result = sumAll(-10, 4);
+console.log(`result = ${result}`);
 // Do not edit below this line
 module.exports = sumAll;
