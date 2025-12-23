5f999a64acbec451a3d05c8dae2c4518bc08281e
Cyber Falcon <thiolimor@gmail.com>
2025-12-23 09:21:30 -0300

    feat(14): Pass fourteenth exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (targetIndex) {
+  if (targetIndex < 0) {
+    return "OOPS";
+  }
+  if (targetIndex == 0) {
+    return 0;
+  }
+  if (targetIndex == 1) {
+    return 1;
+  }
 
+  let beforePrevious = 0;
+  let previous = 1;
+  let result;
+  for (let i = 2; i <= targetIndex; i++) {
+    result = beforePrevious + previous;
+    beforePrevious = previous;
+    previous = result;
+  }
+  return result;
 };
 
+console.log(fibonacci(1));
 // Do not edit below this line
 module.exports = fibonacci;
