34332121521d936694455c98f7d5e911d5f2b885
Cyber Falcon <thiolimor@gmail.com>
2025-12-23 08:32:51 -0300

    feat(13): Pass thirteenth exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  const squashed = removePontuation(input).toLowerCase();
+  const middleOfString = squashed.length / 2 - 1;
+  for (let i = 0; i < middleOfString; i++) {
+    const oppositeLetter = squashed.at(-i - 1);
+    if (squashed[i] != oppositeLetter) {
+      return false;
+    }
+  }
+  return true;
 };
+function removePontuation(text) {
+  return text.replace(/[^a-z0-9]/gi, "");
+}
 
+console.log(palindromes("Racecar"));
 // Do not edit below this line
 module.exports = palindromes;
